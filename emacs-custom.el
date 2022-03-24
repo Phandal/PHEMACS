93e9144202a262740fd66cb0bca55d1556a76354
BaileyCPSG <bailey@buypowersportsgear.com>
2022-03-24 13:42:50 -0500

    Removed errors from manual merge

--- emacs-custom.el
+++ emacs-custom.el
@@ -4,11 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-<<<<<<< HEAD
-   '("9b54ba84f245a59af31f90bc78ed1240fca2f5a93f667ed54bbf6c6d71f664ac" "f6665ce2f7f56c5ed5d91ed5e7f6acb66ce44d0ef4acfaa3a42c7cfe9e9a9013" "cf922a7a5c514fad79c483048257c5d8f242b21987af0db813d3f0b138dfaf53" "e8df30cd7fb42e56a4efc585540a2e63b0c6eeb9f4dc053373e05d774332fc13" "8146edab0de2007a99a2361041015331af706e7907de9d6a330a3493a541e5a6" "846b3dc12d774794861d81d7d2dcdb9645f82423565bfb4dad01204fa322dbd5" "f7fed1aadf1967523c120c4c82ea48442a51ac65074ba544a5aefc5af490893b" "c2aeb1bd4aa80f1e4f95746bda040aafb78b1808de07d340007ba898efa484f5" "7a7b1d475b42c1a0b61f3b1d1225dd249ffa1abb1b7f726aec59ac7ca3bf4dae" "1d5e33500bc9548f800f9e248b57d1b2a9ecde79cb40c0b1398dec51ee820daf" "835868dcd17131ba8b9619d14c67c127aa18b90a82438c8613586331129dda63" "5810e424b9f7cadc9ef956e5ec6c375e814da3306186c0f3ab459abbbabcc76a" "3bddc745807c3eb8f2bebda5edeb3186f67c669a0d9ae401f9fda900814dd8e1" default)))
-=======
-   '("11b05f64c8772cdbc0318e477ae4f95500feb84ff9f857080439b33ae96d0b39" "b732f05b0391fcb3b50e8b79d78c6b903fac7cc894d528eb06408d9bc6da5e67" "6e8e2c856ba00dc6ff7ec72ef409bbf9a72354430a4fe610c51179675ff64b96" "5810e424b9f7cadc9ef956e5ec6c375e814da3306186c0f3ab459abbbabcc76a" "3bddc745807c3eb8f2bebda5edeb3186f67c669a0d9ae401f9fda900814dd8e1" default)))
->>>>>>> d1a5644a1c396dec03b8ecfd84823a905d5b0032
+   '("5810e424b9f7cadc9ef956e5ec6c375e814da3306186c0f3ab459abbbabcc76a" default)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- keybinds.el
+++ keybinds.el
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-(global-set-key (kbd "C-<return>") 'bf/open-line-below)
-(global-set-key (kbd "C-M-<return>") 'bf/open-line-above)
-
 (global-set-key (kbd "C-c m") 'compile)
 (global-set-key (kbd "C-c M-m") 'recompile)
 
@@ -9,11 +5,10 @@
 (global-set-key (kbd "C-c l") 'windmove-right)
 (global-set-key (kbd "C-c k") 'windmove-up)
 (global-set-key (kbd "C-c j") 'windmove-down)
-=======
+
 (global-set-key [f5] 'ph/change-to-emacs-directory)
 (global-set-key [f6] 'ph/edit-user-init-file)
 (global-set-key [f8] 'ph/find-agenda-file)
 (global-set-key [f7] 'ph/load-user-init-file)
 (global-set-key (kbd "C-<return>") 'ph/open-line-below)
 (global-set-key (kbd "C-M-<return>") 'ph/open-line-above)
->>>>>>> d1a5644a1c396dec03b8ecfd84823a905d5b0032

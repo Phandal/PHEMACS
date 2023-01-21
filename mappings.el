069b65658c7004a612afb2ddccb734594ecedc60
Phandal <bfeld.111@gmail.com>
2023-01-21 10:04:18 -0600

    Bound key to modus-themes-toggle

--- mappings.el
+++ mappings.el
@@ -2,6 +2,7 @@
 (global-set-key (kbd "<escape>") 'keyboard-quit)
 (global-set-key (kbd "C-c c") 'compile)
 (global-set-key (kbd "C-c C-t") 'treemacs)
+(global-set-key (kbd "<f5>") 'modus-themes-toggle)
 
 ;; Unsetting keybinds
 (global-unset-key (kbd "C-x C-z"))
